c8ecfbb38915b397816c92a9bc99b02481fe0006
Asraful <asrafulislamcodes@gmail.com>
2023-03-08 01:30:36 +0600

    fixed some

--- pages/_app.js
+++ pages/_app.js
@@ -5,23 +5,16 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { SessionProvider } from 'next-auth/react';
-
 import '@/styles/globals.scss';
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
+export default function App({ Component, pageProps }) {
   const queryClient = React.useRef(new QueryClient());
   return (
-    <SessionProvider session={session}>
-      <QueryClientProvider client={queryClient.current}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
-        </Hydrate>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
